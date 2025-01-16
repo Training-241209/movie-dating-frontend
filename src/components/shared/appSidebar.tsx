929f51f460aa581deaf26a1f6aa5a9b4f2e05846
dchelbig <dchelbig@gmail.com>
2025-01-16 12:22:03 -0500

    First commit

---
+++ src/components/shared/appSidebar.tsx
@@ -0,0 +1,100 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarFooter,
+} from "@/components/ui/sidebar"
+import { Button } from "../ui/button"
+import { useQueryClient } from "@tanstack/react-query";
+import { Link, useRouter } from "@tanstack/react-router";
+import { useAuth } from "@/features/hooks/use-Auth";
+import { useState,useEffect } from "react";
+import { useGetChatRooms } from "@/features/hooks/use-getChatRooms";
+import { useLogout } from "@/features/hooks/use-logout";
+import logo from "@/assets/cinematchLogo.png"
+
+export function AppSidebar()  {
+  
+  const queryClient = useQueryClient();
+  const router = useRouter()
+  const {mutate: logout} = useLogout();
+
+  function onChat(senderId:string|undefined,recipientId:string){
+    router.navigate({
+      to: `/chat/${senderId}/${recipientId}`,
+      params: { senderId, recipientId },
+    });
+  }
+  const {data: auth} = useAuth()
+  
+  const {data:getChatRooms=[], isPending,refetch} = useGetChatRooms()
+
+  useEffect(() => {
+    console.log("getChatRooms ",getChatRooms)
+    refetch()
+  }, [refetch]);
+  
+  return (
+    <div>
+      <Sidebar >
+      <img src={logo} alt="Logo" className="w-[130px]"/>
+      <SidebarContent className="">
+        <SidebarGroup>
+        <SidebarGroupContent >
+          <div className="flex items-center justify-center text-xl font-bold w-full">
+            {auth?.firstName + " " +
+            auth?.lastName}
+          </div>
+        
+          </SidebarGroupContent>
+          <SidebarGroupLabel className="flex text-l font-bold">Settings</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <Link to="/genreSelection">
+              <SidebarMenuButton>
+                Update Genre
+              </SidebarMenuButton>
+              </Link>
+              <Link to="/genderSelection">
+              <SidebarMenuButton>
+                Update Gender
+              </SidebarMenuButton>
+              </Link>              
+              <Link to="/credential">
+              <SidebarMenuButton>
+                Update Credentials
+              </SidebarMenuButton>
+              </Link>    
+              
+            </SidebarMenu>
+          </SidebarGroupContent>
+          <SidebarGroupLabel className="flex text-l font-bold">Conversations</SidebarGroupLabel>
+          <SidebarGroupContent>
+          {!isPending ?(
+            getChatRooms.length > 0 ? (
+              getChatRooms.map((item:any) => (
+                <SidebarMenuButton key={item.username} onClick={() => onChat(auth?.username, item.username)}>
+                  <li>
+                    {item.username}
+                  </li>
+                </SidebarMenuButton>
+              ))
+            ) : (
+              "No ChatRoom Available")
+          ) : "Loading"
+          }
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <Button type = 'submit' onClick={() =>logout()}>Log Out</Button>
+      </SidebarFooter>
+    </Sidebar>
+    </div>
+    
+  )
+}
---
+++ src/features/hooks/use-getMovies.ts
@@ -0,0 +1,16 @@
+import { useQuery, } from "@tanstack/react-query";
+import { axiosInstanceExternal } from "@/lib/axios-Externalconfig";
+import { useRouter } from "@tanstack/react-router";
+export function useGetMovies({genreId}:{genreId:number}){
+    return (
+        useQuery({
+            queryKey: ['movie'],
+            queryFn: async () => {
+                console.log("GENRE",genreId)
+                const response = await axiosInstanceExternal.get(`/3/discover/movie?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}&with_original_language=en`);
+                console.log(response.data.results)
+                return response.data.results;
+            }
+        }) 
+    )
+}
