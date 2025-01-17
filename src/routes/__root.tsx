1cc7d620ff3d7d00c91ad1f5395e06950d4668ad
CarlosA03 <Carlosalvizo03@gmail.com>
2025-01-16 22:28:47 -0600

    commented stomp stuff in root and registerform

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,12 +3,12 @@ import QueryProvider from '@/providers/query-provider'
 import { createRootRoute, Outlet} from '@tanstack/react-router'
 
 
+
 export const Route = createRootRoute({
   component: () => {
 
     return (
 
-            
           <QueryProvider>
           <div className='min-h-screen bg-red-300 '>
             <Outlet />
