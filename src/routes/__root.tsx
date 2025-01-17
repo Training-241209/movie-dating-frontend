701c967ac869509f83e166bcad9715f7a07c85a4
CarlosA03 <Carlosalvizo03@gmail.com>
2025-01-16 22:20:57 -0600

    ws

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from '@/components/ui/sonner'
 import QueryProvider from '@/providers/query-provider'
 import { createRootRoute, Outlet} from '@tanstack/react-router'
-import { StompSessionProvider } from 'react-stomp-hooks'
 
 
 export const Route = createRootRoute({
