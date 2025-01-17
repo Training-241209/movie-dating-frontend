bfc17fc8db87a36d67c293d6a85cc273597f1af1
CarlosA03 <Carlosalvizo03@gmail.com>
2025-01-17 00:11:17 -0600

    hook fix

--- src/features/hooks/use-getChatRooms.ts
+++ src/features/hooks/use-getChatRooms.ts
@@ -6,7 +6,7 @@ export function useGetChatRooms() {
     return useQuery({
         queryKey: ['chatRooms'],
         queryFn: async() =>{
-              const response = await axiosInstance.get(`http://localhost:8080/api/match/${auth?.username}`)
+              const response = await axiosInstance.get(`/api/match/${auth?.username}`)
               console.log("DATA ",response.data)
               return response.data;
 
--- src/features/hooks/use-getChats.ts
+++ src/features/hooks/use-getChats.ts
@@ -5,7 +5,7 @@ export function useGetChats({ sender, recipient }: { sender: string; recipient: 
   return useQuery({
     queryKey: ['chats', sender, recipient],
     queryFn: async () => {
-      const response = await axiosInstance.get(`http://localhost:8080/messages/${sender}/${recipient}`);
+      const response = await axiosInstance.get(`/messages/${sender}/${recipient}`);
       console.log(response.data);
       return response.data;
     },
